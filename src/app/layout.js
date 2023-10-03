4453cd9e7d48e54ce24a95652d97b4d121d464c8
mahdee18 <mahdeec10@gmail.com>
2023-10-03 12:32:22 +0600

    Added Navbar and Footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
+import Navbar from '@/components/Navbar'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Footer from '@/components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,13 +14,9 @@ const RootLayout = ({ children }) => {
   return (
     <html lang="en">
       <body className={inter.className}>
-      <nav>
-        <a>Home</a>
-      </nav>
+      <Navbar />
       {children}
-      <footer>
-        Footer
-      </footer>
+      <Footer />
       
       </body>
     </html>
